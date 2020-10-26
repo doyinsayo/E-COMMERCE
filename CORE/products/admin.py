54ade11717b54582fd991a1beb279e615a183e12
Doyinsayo Otufadebo <doyinsa01@gmail.com>
2020-10-26 19:34:33 +0100

    migrations

--- CORE/products/admin.py
+++ CORE/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
+from . models import Category,Product
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
