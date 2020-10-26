18271ea4feba1bb27d53bb953ef247ed4b78ba9a
Doyinsayo Otufadebo <doyinsa01@gmail.com>
2020-10-26 19:25:51 +0100

    pillow image library

--- CORE/products/models.py
+++ CORE/products/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=300)
+    primaryCategory = models.BooleanField(default=False)
 
-class Product(models.Model):
+    def __str__(self):
+        return self.title
+
+#Product Model
+class  Product(models.Model):
+    mainimage = models.ImageField(upload_to='products/', blank=True)
+    name = models.CharField(max_length=300)
+    slug = models.SlugField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    preview_text = models.TextField(max_length=200, verbose_name='Preview Text')
+    detail_text = models.TextField(max_length=1000, verbose_name='Detail Text')
+    price = models.FloatField()
     
+
+    def __str__(self):
+        return self.name        
+
