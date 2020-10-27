9ddb7ccb2f80689e843d1ca0962bf380182f4ffc
Doyinsayo Otufadebo <doyinsa01@gmail.com>
2020-10-27 22:22:42 +0100

    Update views.py

--- CORE/cart/views.py
+++ CORE/cart/views.py
@@ -65,37 +65,7 @@ def remove_from_cart(request, slug):
         messages.info(request, "You do not have an active order")
         return redirect("products:home")        
 
-Django eCommerce tutorial part two django allauth
-Posted 11. September 2019. 17 min read.
-Cover image
-In the last part, we stopped at Adding Items to Cart and Removing items from cart. Now in this part we are going to learn managing every single items in the cart, and in Order . Use Django-allauth to quickly add the Register and Login ability in our Django ecommerce project to the user so they can add the item to the cart.
 
-In this post you may be see some more html part rather than python codes. We are going to use Bootstrap a css framework for rapid design. If you don’t know Bootstrap, then you should check out the amazing tutorial on w3schools or Bootstap official documentation.
-
-Learn Bootstrap is not necessary, but having the knowledge of HTML and CSS will help you make the design look better. For this tutorial I am going to link the GitHub repository so you can download the code and practice.
-
-http://bit.ly/Django-eCommerce-Github-Repo
-
-What we are going to learn in this blog post :
-1. **Adding ability to increase and decrease the Product quantity** 2. **Showing the Cart Items in the Cart Page** 3. **Designing our website using Bootstrap** You can skip this part if you don’t like 4. **Adding Registration and Login Feature in our Django Project**
-If you missed the previous part of this tutorial series, you can either go back to home page of this blog or use this links to navigate to the corresponding tutorial.
-Adding search filters in Django eCommerce website
-Real world SAAS application in Django tutorial
-Build a SaaS Application in Django 3.0| SaaS Series
-Stripe Payment Gateway Integration in Django eCommerce Website
-Django eCommerce tutorial part two
-So. let’s start the tutorial step by step as we do. Grab a cup of coffee ☕ and start coding.
-
-1. Showing the Cart Items in the Cart Page
-In the last part we were able to add items to the cart and in Order but we are able to see that which item we have in the cart and how many quantities we have.
-
-Before we get started make sure you clone the repo from github >>
-
-repo link
-
-For showing the cart items to the corresponding user, we need to create view for the Cart View Page. So, the basic stuff, create a new view in the Cart app, we are calling the view as CartView and we are going to use Function based views for now but letter on we are going to convert this view in Class Based View
-
-CartView Code :
 
 # Cart View
 def CartView(request):
